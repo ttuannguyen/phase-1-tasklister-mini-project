6a840400c581a8c712613b5dd9d7193968cd2ae9
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-01-22 17:28:34 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+//The following is the MVP 
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  
+  //click some form of a submit button:
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskInput();
+  })
 });
+
+//to type a task into the input field: 
+const taskInput = () => {
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement('li');
+  newTask.textContent = newTaskDescription.value; //innerText also works
+  //document.querySelector('#tasks').appendChild(newTask);
+  toAppend(newTask)
+}
+
+const toAppend = task => {
+  document.getElementById("tasks").appendChild(task);
+}
+
